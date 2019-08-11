cc98e8ac1deb1b19f3caf3d7fe4296b3fe28eefe
Anton Sivagin <ansn.onward@gmail.com>
2019-08-11 21:26:27 +0300

    создал три файла

---
+++ script.js
@@ -0,0 +1,81 @@
+var create = document.querySelector('.header__create');
+var createMenu = document.querySelector('.header__menu');
+var createButton = document.querySelector('.btn--create');
+var menu = document.querySelector('.header__menu');
+var exit = document.querySelector('.exit__btn');
+var content = document.querySelector('.page__main');
+var input = document.querySelector('.name');
+var cancel = document.querySelector('.btn--cancel');
+var problem = document.querySelector('.exit__problem');
+
+create.addEventListener('click', function() {
+    menu.style.display = "flex";
+    create.style.display = "none"
+})
+
+document.addEventListener('click', function(event) {
+    if (!createMenu.contains(event.target)) e.style.display='none';
+  });
+
+exit.addEventListener('click', function() {
+    menu.style.display = "none";
+    create.style.display = "flex"
+    document.querySelector('.name').value='';
+})
+
+cancel.addEventListener('click', function() {
+    menu.style.display = "none";
+    create.style.display = "flex"
+    document.querySelector('.name').value='';
+})
+
+function deleteButton() {
+    // var content = document.querySelector('.main__exit');
+    // var yes = document.createElement('div');
+    // var no = document.createElement('div');
+    // var exit = document.createElement('div')
+    // exit.className = "exit__problem problem"
+    // yes.className = "problem__btn yes";
+    // yes.innerHTML = 'Yes';
+    // no.className = "problem__btn no";
+    // no.innerHTML = 'No';
+    // console.log(1)
+    // exit.appendChild(content);
+    // yes.appendChild(content);    
+    // no.appendChild(content);
+    this.parentNode.parentNode.removeChild(this.parentNode);
+}
+
+function closingButton() {
+    var j = document.createElement('div');
+    j.className = "main__exit exit";
+    j.innerHTML = '+';
+    j.addEventListener('click', deleteButton)
+    this.appendChild(j);
+}
+
+function createBoard() {
+    var i = document.createElement('div');
+    i.innerHTML = document.querySelector('.name').value;
+    i.className = "main__btn btn";
+    i.addEventListener('mouseover', closingButton, {
+        once: "true",
+    })
+    i.addEventListener('mouseover', function() {
+        i.classList.add('main__btn--hover');
+    })
+    i.addEventListener('mouseout', function() {
+        i.classList.remove('main__btn--hover');
+    })
+    content.appendChild(i);
+    document.querySelector('.name').value='';
+}
+
+createButton.addEventListener('click', createBoard) 
+
+input.addEventListener('keydown', function() {
+    if (event.keyCode == 13) {
+        createBoard()
+    }
+})
+
